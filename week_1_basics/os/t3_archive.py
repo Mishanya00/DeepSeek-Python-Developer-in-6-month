0bd846a369c1ef79b168ac8c72a82fd5b28367e9
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-03-23 21:26:17 +0300

    VSCode set up. Refactoring

--- week_1_basics/os/t3_archive.py
+++ week_1_basics/os/t3_archive.py
@@ -1,30 +1,32 @@
 import os
-import shutil
+
+# import shutil
 from zipfile import ZipFile
 from datetime import datetime
 
-'''
+"""
 Write a program that creates a backup of a specified directory by archiving it into a `.zip` file.'
-'''
+"""
+
 
 def archive_directory(source, dest):
-    '''Archives source directory'''
+    """Archives source directory"""
 
     if not os.path.isdir(source):
-        print('Error: Source path is not a folder or not exist!')
+        print("Error: Source path is not a folder or not exist!")
         return False
     if not os.path.isdir(dest):
-        print('Error: Destination path is not a folder or not exist!')
+        print("Error: Destination path is not a folder or not exist!")
         return False
-    
     curr_time = datetime.now()
-    timestamp = curr_time.strftime('%Y-%m-%d_%H-%M-%S')
-    backup_filename = dest + '\\' + 'backup_{}'.format(timestamp) + '.zip'
+    timestamp = curr_time.strftime("%Y-%m-%d_%H-%M-%S")
+    backup_filename = dest + "\\" + f"backup_{timestamp}" + ".zip"
 
-    with ZipFile(backup_filename, 'w') as fzip:
+    with ZipFile(backup_filename, "w") as fzip:
         for path, _, files in os.walk(source):
             for file in files:
-                fzip.write(path + '\\' + file)
+                fzip.write(path + "\\" + file)
     return True
 
-archive_directory('test', 'test2')
+
+archive_directory("test", "test2")
