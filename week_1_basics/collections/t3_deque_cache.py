f356ad9c01e6c7461b53ac938b49e1b4d93b9e31
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-03-27 17:37:21 +0300

    collections: cache task is done

--- week_1_basics/collections/t3_deque_cache.py
+++ week_1_basics/collections/t3_deque_cache.py
@@ -1,20 +1,44 @@
 from collections import deque
 
 
-class CacheObject:
-
-    def __init__(self, value):
-        self.value = value
-
 class DequeCache:
 
     def __init__(self, capacity):
         self._capacity = capacity
         self._elements = deque()
 
-    def add(self, obj: CacheObject):
-        if len(self._elements <= self._capacity):
+    def __getitem__(self, index):
+        res = self._elements[index]
+        self._elements.remove(self._elements[index])
+        self._elements.append(res)
+        return res
+    
+    def __repr__(self):
+        return ', '.join(list(map(str, self._elements)))
+
+    def add(self, obj):
+        if obj in self._elements:
+            self._elements.remove(obj)
             self._elements.append(obj)
         else:
-            self._elements.popleft()
-            self._elements.append(obj)
+            if len(self._elements) <= self._capacity:
+                self._elements.append(obj)
+            else:
+                self._elements.popleft()
+                self._elements.append(obj)
+
+cache = DequeCache(5)
+
+for i in range(20):
+    print(cache)
+    cache.add(i)
+
+print(cache)
+
+print('Here is get:')
+cache[2]
+print(cache)
+cache[2]
+print(cache)
+cache[2]
+print(cache)
