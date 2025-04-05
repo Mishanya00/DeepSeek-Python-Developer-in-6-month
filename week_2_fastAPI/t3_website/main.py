6d655a3daf59b3fd0a74fda5d04e1d302e9f63b4
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-04-05 09:12:58 +0300

    Real file uploading. BackgroundTasks

--- week_2_fastAPI/t3_website/main.py
+++ week_2_fastAPI/t3_website/main.py
@@ -1,27 +1,44 @@
 from typing import Annotated
 
+import os
+
+import asyncio
+
 import chardet
 
-from fastapi import FastAPI, Request, Form, File, UploadFile
+from fastapi import FastAPI, Request, Form, File, UploadFile, HTTPException, BackgroundTasks
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+
+
+EMAIL_PATTERN = r"\b[\w\-\.]+@(?:[\w-]+\.)+[\w\-]{2,4}\b"
+UPLOAD_DIR = "file_storage"
+
+
+if not os.path.exists(UPLOAD_DIR):
+    os.mkdir(UPLOAD_DIR)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-EMAIL_PATTERN = r"\b[\w\-\.]+@(?:[\w-]+\.)+[\w\-]{2,4}\b"
+
+def secure_filename(filename: str) -> str:
+    return os.path.basename(filename).replace(" ", "_")  # Functionality may be extended
+
+
+async def delete_user_file(filename: str, delay: int):
+    await asyncio.sleep(delay)
+    try:
+        os.remove(filename)
+    except FileNotFoundError:
+        pass
 
 
 @app.get("/", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
-
-
-# @app.get("/test")
-# async def test_test(test_str: str):
-#     return {"test": test_str}
 
 
 @app.post("/login")
@@ -42,20 +59,31 @@ async def dashboard_page(request: Request, email: str = None):
     )
 
 
+# FastAPI will create the object of type BackgroundTasks and pass it as that parameter.
 @app.post("/uploadfile")
-async def create_upload_file(file: Annotated[UploadFile | str, File()] = None):
+async def create_upload_file(background_tasks: BackgroundTasks, file: Annotated[UploadFile | str, File()] = None):
     if not file:
         return {"message": "No upload file sent"}
     elif file.content_type != "text/plain":
         return {"message": "Not a text file!"}
     else:
-        contents = await file.read()
-        encoding = chardet.detect(contents)['encoding']
-        print('TEXT ENCODING: ' + str(encoding))
-        decoded = contents.decode(encoding or "utf-8", errors="replace")
-        return {"filename": file.content_type, "contents": decoded}
+        try:
+            contents = await file.read()
+            encoding = chardet.detect(contents)["encoding"]
+            decoded = contents.decode(encoding or "utf-8", errors="replace")
 
+            path = UPLOAD_DIR + "/" + secure_filename(file.filename)
+            print(path)
 
-# @app.post("/uploadfile/")
-# async def create_upload_file(file: Annotated[bytes, File()]):
-#     return {"file_size": len(file)}
+            with open(path, "w", encoding="utf-8") as f:
+                f.write(decoded)
+
+            background_tasks.add_task(delete_user_file, path, 3600)
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e)) from e
+        finally:
+            await file.close()
+
+        return {"message": f"file {file.filename} has been successfully uploaded."}
+    
