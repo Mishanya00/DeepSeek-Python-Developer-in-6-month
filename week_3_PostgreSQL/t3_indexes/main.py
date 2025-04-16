26e6acf6a89ebdaff2bf4c5104c07a60904d4f37
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-04-16 23:55:36 +0300

    10k User creation & selection time analyzer User creation a little longer for indexed table. User selection 4x faster for 10k users in indexed table

--- week_3_PostgreSQL/t3_indexes/main.py
+++ week_3_PostgreSQL/t3_indexes/main.py
@@ -7,6 +7,11 @@ from names import popular_names
 from random import choice
 
 
+random_names = [choice(popular_names) for i in range(10**4)]
+random_names_to_select1 = [choice(popular_names) for i in range(10**4)]
+random_names_to_select2 = [choice(popular_names[100:375:]) for i in range(10**4)]   # Not all possible names
+
+
 async def drop_table(pool: asyncpg.Pool):
     async with pool.acquire() as aconn:
         await aconn.execute('DROP TABLE IF EXISTS users')
@@ -23,6 +28,18 @@ async def create_table(pool: asyncpg.Pool):
                         )
                         """
         )
+
+
+async def create_index(pool: asyncpg.Pool):
+    async with pool.acquire() as aconn:
+        # await aconn.execute('CREATE INDEX IF NOT EXISTS idx_name ON users USING HASH(name)')
+        await aconn.execute('CREATE INDEX IF NOT EXISTS idx_name ON users(name)')
+
+
+async def select_user(pool: asyncpg.Pool, name: str):
+    async with pool.acquire() as aconn:
+        result = await aconn.fetchrow('SELECT id, name, signup_date FROM users WHERE name = $1', name)
+        return result
     
 
 async def create_user(pool: asyncpg.Pool, name: str):
@@ -33,10 +50,18 @@ async def create_user(pool: asyncpg.Pool, name: str):
 
 async def create_10k_users(pool: asyncpg.Pool):
     start = datetime.now()
-    for i in range(10**4):
-        await create_user(pool, choice(popular_names))
+    for name in random_names:
+        await create_user(pool, name)
     finish = datetime.now()
     print('Time to add 10k users:', finish-start)
+
+
+async def select_10k_random_users(pool: asyncpg.Pool):
+    start = datetime.now()
+    for name in random_names_to_select2:
+        await select_user(pool, choice(name))
+    finish = datetime.now()
+    print('Time to select 10k random users:', finish-start)
 
 
 async def main():
@@ -44,8 +69,18 @@ async def main():
 
     asyncpg_pool = await asyncpg.create_pool(conn_string)
 
+    print('---------------- INDEXED OPERATIONS ----------------')
     await drop_table(asyncpg_pool)
     await create_table(asyncpg_pool)
+    await create_index(asyncpg_pool)
+    await create_10k_users(asyncpg_pool)
+    await select_10k_random_users(asyncpg_pool)
+
+    print('--------------- NON INDEXED OPERATIONS ---------------')
+    await drop_table(asyncpg_pool)
+    await create_table(asyncpg_pool)
+    await create_10k_users(asyncpg_pool)
+    await select_10k_random_users(asyncpg_pool)
 
 
 asyncio.run(main())
